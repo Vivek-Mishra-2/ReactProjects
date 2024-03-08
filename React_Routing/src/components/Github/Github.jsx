b139f9df445c56a535060cdf90d00bb9b2d01d26
Vivek Mishra <vivek.mishra@s.amity.edu>
2024-03-08 19:51:38 +0530

    Updated Github file

--- React_Routing/src/components/Github/Github.jsx
+++ React_Routing/src/components/Github/Github.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 function Github() {
   const data = useLoaderData()
   return (
-    <div className='text-center m-4 bg-yellow-300 text-white p-4 text-3xl'>Github Followers: {data.followers ? data.followers : "Data not found"}
+    <div className='text-center m-4 bg-blue-200 text-black p-4 text-3xl rounded-3xl'>Github Followers: {data.followers ? data.followers : "Data not found"}
     <img src={data.avatar_url ? data.avatar_url : "Data not found"} alt='avatar' className='rounded-full w-32 h-32 mx-auto mt-4' />
     </div>
   )
@@ -12,6 +12,6 @@ function Github() {
 export default Github
 
 export const githubInfo = async () => {
-  const response = await fetch("https://api.github.com/users/hiteshChoudhary")
+  const response = await fetch(`https://api.github.com/users/vivek-mishra-2`)
   return response.json()
 }
